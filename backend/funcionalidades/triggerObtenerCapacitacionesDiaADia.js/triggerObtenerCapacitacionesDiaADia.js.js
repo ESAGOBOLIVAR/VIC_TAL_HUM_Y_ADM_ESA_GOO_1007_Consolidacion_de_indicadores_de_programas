90d576ae4810e85484784e784aacb7fd01bfbdc3
Mauricio <esago@segurosbolivar.com>
2023-06-30 14:32:40 -0500

    se actualiza el archivo comentando las lineas

--- backend/funcionalidades/triggerObtenerCapacitacionesDiaADia.js/triggerObtenerCapacitacionesDiaADia.js.js
+++ backend/funcionalidades/triggerObtenerCapacitacionesDiaADia.js/triggerObtenerCapacitacionesDiaADia.js.js
@@ -1,130 +1,130 @@
-//funcion para obtener las capacitaciones del dia a dia en base a la marca temporal etc..
-function triggerObtenerCapacitacionesDiaADia() {
-  const { idDataBase, nameTables } = parametrosGlobales();
-  const { idBaseDeDatosMasterIndicadores, idBaseDeDatosEncuesta } = idDataBase;
-  const { tablaMasterDiario, tablaRespuestasDeFormulario1 } = nameTables;
+// //funcion para obtener las capacitaciones del dia a dia en base a la marca temporal etc..
+// function triggerObtenerCapacitacionesDiaADia() {
+//   const { idDataBase, nameTables } = parametrosGlobales();
+//   const { idBaseDeDatosMasterIndicadores, idBaseDeDatosEncuesta } = idDataBase;
+//   const { tablaMasterDiario, tablaRespuestasDeFormulario1 } = nameTables;
 
-  //data de donde se almacenan las respuestas del google form
-  let dataRespuestasDeFormulario1 = readAllArray([
-    tablaRespuestasDeFormulario1,
-    idBaseDeDatosEncuesta,
-  ]);
+//   //data de donde se almacenan las respuestas del google form
+//   let dataRespuestasDeFormulario1 = readAllArray([
+//     tablaRespuestasDeFormulario1,
+//     idBaseDeDatosEncuesta,
+//   ]);
 
-  dataRespuestasDeFormulario1 = JSON.parse(dataRespuestasDeFormulario1);
+//   dataRespuestasDeFormulario1 = JSON.parse(dataRespuestasDeFormulario1);
 
-  //obtener data del master diario
-  let dataMasterDiario = readAllArray([
-    tablaMasterDiario,
-    idBaseDeDatosMasterIndicadores,
-  ]);
+//   //obtener data del master diario
+//   let dataMasterDiario = readAllArray([
+//     tablaMasterDiario,
+//     idBaseDeDatosMasterIndicadores,
+//   ]);
 
-  //recorrer data del master diario
+//   //recorrer data del master diario
 
-  dataMasterDiario = JSON.parse(dataMasterDiario);
+//   dataMasterDiario = JSON.parse(dataMasterDiario);
 
-  dataMasterDiario.map((el, indice) => {
-    // let categoria = el[1];
-    let programa = el[2];
-    let poblacion = el[3];
-    let fechaInicio = el[13].split("T")[0];
+//   dataMasterDiario.map((el, indice) => {
+//     // let categoria = el[1];
+//     let programa = el[2];
+//     let poblacion = el[3];
+//     let fechaInicio = el[13].split("T")[0];
 
-    let estado = el[15];
-    let estadoEsago = el[26];
+//     let estado = el[15];
+//     let estadoEsago = el[26];
 
-    fechaInicio = limpiarFecha(fechaInicio);
+//     fechaInicio = limpiarFecha(fechaInicio);
 
-    // si el estado es planeacio entonces tomar ese registro
-    // filtrar donde la fecha de inicio sea igual al a de la encuesta la poblacion y el programa
-    if (estado == "EN PLANEACIÓN" && estadoEsago != "PROCESADO") {
-      // console.log(el);
-      let filtroDia = dataRespuestasDeFormulario1.filter(
-        (el) =>
-          limpiarFecha(el[0].split("T")[0]) == fechaInicio &&
-          el[5].trim() == poblacion.trim() &&
-          el[6].trim() == programa.trim()
-      );
+//     // si el estado es planeacio entonces tomar ese registro
+//     // filtrar donde la fecha de inicio sea igual al a de la encuesta la poblacion y el programa
+//     if (estado == "EN PLANEACIÓN" && estadoEsago != "PROCESADO") {
+//       // console.log(el);
+//       let filtroDia = dataRespuestasDeFormulario1.filter(
+//         (el) =>
+//           limpiarFecha(el[0].split("T")[0]) == fechaInicio &&
+//           el[5].trim() == poblacion.trim() &&
+//           el[6].trim() == programa.trim()
+//       );
 
-      if (filtroDia.length > 0) {
-        //filtro de dia es igual a un arreglo de arreglos [[data,data],[data,data]]
+//       if (filtroDia.length > 0) {
+//         //filtro de dia es igual a un arreglo de arreglos [[data,data],[data,data]]
 
-        console.log(filtroDia);
-        // console.log(filtroDia);
-        let nroAsistentesQueLleganASesion = filtroDia.length;
+//         console.log(filtroDia);
+//         // console.log(filtroDia);
+//         let nroAsistentesQueLleganASesion = filtroDia.length;
 
-        let utilidad = calcularUtilidad(filtroDia);
+//         let utilidad = calcularUtilidad(filtroDia);
 
-        console.log("UTILIDAD" + utilidad);
-        console.log("NRO ASISTENTES" + nroAsistentesQueLleganASesion);
+//         console.log("UTILIDAD" + utilidad);
+//         console.log("NRO ASISTENTES" + nroAsistentesQueLleganASesion);
 
-        utilidad = parseInt(utilidad) / parseInt(nroAsistentesQueLleganASesion);
+//         utilidad = parseInt(utilidad) / parseInt(nroAsistentesQueLleganASesion);
 
-        console.log("UTILIDAD DIVISION" + utilidad);
+//         console.log("UTILIDAD DIVISION" + utilidad);
 
-        let numeroRedondeadoUtilizad = redondearNumero(utilidad);
+//         let numeroRedondeadoUtilizad = redondearNumero(utilidad);
 
-        let [cantidadPromotores, cantidadDetratores] = calcularNps(filtroDia);
+//         let [cantidadPromotores, cantidadDetratores] = calcularNps(filtroDia);
 
-        console.log("CANTIDAD DE PROMOTORES" + cantidadPromotores);
-        console.log("CANTIDAD DE DETRATORES" + cantidadDetratores);
+//         console.log("CANTIDAD DE PROMOTORES" + cantidadPromotores);
+//         console.log("CANTIDAD DE DETRATORES" + cantidadDetratores);
 
-        //actualizar celda T del registro
-        //si encuentra la busqueda entonces
-        let columna = 20;
-        let fila = indice + 2;
+//         //actualizar celda T del registro
+//         //si encuentra la busqueda entonces
+//         let columna = 20;
+//         let fila = indice + 2;
 
-        let columnaEstadoEsago = 27;
+//         let columnaEstadoEsago = 27;
 
-        let columnaUtilizad = 25;
+//         let columnaUtilizad = 25;
 
-        let columnaCantidadPromotores = 28;
-        let columnaCantidadDetratores = 29;
+//         let columnaCantidadPromotores = 28;
+//         let columnaCantidadDetratores = 29;
 
-        updateField([
-          tablaMasterDiario,
-          idBaseDeDatosMasterIndicadores,
-          fila,
-          columna,
-          nroAsistentesQueLleganASesion,
-        ]);
+//         updateField([
+//           tablaMasterDiario,
+//           idBaseDeDatosMasterIndicadores,
+//           fila,
+//           columna,
+//           nroAsistentesQueLleganASesion,
+//         ]);
 
-        updateField([
-          tablaMasterDiario,
-          idBaseDeDatosMasterIndicadores,
-          fila,
-          columnaEstadoEsago,
-          "PROCESADO",
-        ]);
+//         updateField([
+//           tablaMasterDiario,
+//           idBaseDeDatosMasterIndicadores,
+//           fila,
+//           columnaEstadoEsago,
+//           "PROCESADO",
+//         ]);
 
-        updateField([
-          tablaMasterDiario,
-          idBaseDeDatosMasterIndicadores,
-          fila,
-          columnaUtilizad,
-          numeroRedondeadoUtilizad,
-        ]);
+//         updateField([
+//           tablaMasterDiario,
+//           idBaseDeDatosMasterIndicadores,
+//           fila,
+//           columnaUtilizad,
+//           numeroRedondeadoUtilizad,
+//         ]);
 
-        updateField([
-          tablaMasterDiario,
-          idBaseDeDatosMasterIndicadores,
-          fila,
-          columnaCantidadPromotores,
-          cantidadPromotores,
-        ]);
+//         updateField([
+//           tablaMasterDiario,
+//           idBaseDeDatosMasterIndicadores,
+//           fila,
+//           columnaCantidadPromotores,
+//           cantidadPromotores,
+//         ]);
 
-        updateField([
-          tablaMasterDiario,
-          idBaseDeDatosMasterIndicadores,
-          fila,
-          columnaCantidadDetratores,
-          cantidadDetratores,
-        ]);
+//         updateField([
+//           tablaMasterDiario,
+//           idBaseDeDatosMasterIndicadores,
+//           fila,
+//           columnaCantidadDetratores,
+//           cantidadDetratores,
+//         ]);
 
-        // let personasQueSeEsperaQueAsistan =
-        //   nroInvitados / nroAsistentesQueLleganASesion;
-      }
-    }
-  });
-}
+//         // let personasQueSeEsperaQueAsistan =
+//         //   nroInvitados / nroAsistentesQueLleganASesion;
+//       }
+//     }
+//   });
+// }
 
 // //funcion calcular la utilizada
 // //@param {Array of Array} filtroDia: es arreglo de arreglo de los registros donde se hace coincidencia
